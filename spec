b79d400b7ae887c4f5544bfe928484c7a39d2301
pocke <p.ck.t22@gmail.com>
2013-05-18 18:02:55 +0900

    first commit

---
+++ spec
@@ -0,0 +1,9 @@
+---
+slug: :tweet_stack
+depends:
+  mikutter: 0.2.2.1230
+  plugin: []
+version: '1.0'
+author: p_ck_
+name: tweet_stack
+description: ツイートを一時的に退避させます。
---
+++ tweet_stack.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:tweet_stack) do
+  stack = nil
+  class String
+    def sys_mes
+      Plugin.call(:update, nil, [Message.new(:message => self, :system => true)])
+    end
+  end
+  command(:tweet_stack,
+          name: 'tweet stack',
+	  condition: lambda{ |opt| true },
+	  visible: true,
+	  role: :postbox) do |opt|
+    post_val = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
+
+    if stack == nil then
+      if post_val != '' then
+        stack = post_val
+        Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ''
+	"「#{stack}」をstackしました。".sys_mes
+      else
+        '投稿ボックスが空なのでstackできません。'.sys_mes
+      end
+    else
+      if post_val == '' then
+        Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = stack
+	stack = nil
+      else
+        '投稿ボックスが空でないのでstackを戻せません。'.sys_mes
+      end
+    end
+  end
+end
