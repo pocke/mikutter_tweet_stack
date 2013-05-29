32a16d60795009a01bb373770064b1b557a5c0b6
pocke <p.ck.t22@gmail.com>
2013-05-29 18:40:47 +0900

    スタックになった

--- tweet_stack.rb
+++ tweet_stack.rb
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create(:tweet_stack) do
-  stack = nil
-  class String
-    def sys_mes
-      Plugin.call(:update, nil, [Message.new(:message => self, :system => true)])
-    end
-  end
-  command(:tweet_stack,
-          name: 'tweet stack',
-          condition: lambda{ |opt| true },
-          visible: true,
-          role: :postbox) do |opt|
-    post_val = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
 
-    if stack == nil then
-      if post_val != '' then
-        stack = post_val
-        Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ''
-        "「#{stack}」をstackしました。".sys_mes
-      else
-        '投稿ボックスが空なのでstackできません。'.sys_mes
-      end
+  stack = Array.new
+  def sys_mes mes
+    Plugin.call(:update, nil, [Message.new(:message => mes, :system => true)])
+  end
+
+  command(:tweet_stack,
+           name: 'tweet stack',
+           condition: lambda{ |opt| true },
+           visible: true,
+           role: :postbox) do |opt|
+    post_val = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
+    if post_val != '' then
+      stack << post_val
+      Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ''
+      sys_mes "「#{stack}」が積まれています"
     else
-      if post_val == '' then
-        Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = stack
-        stack = nil
+      if stack[0] then
+        Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = stack.pop
       else
-        '投稿ボックスが空でないのでstackを戻せません。'.sys_mes
+        sys_mes 'stackが空です'
       end
     end
   end
+
 end
