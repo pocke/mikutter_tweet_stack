e804c59a3cb963466b13414acf8521f7eb992794
pocke <p.ck.t22@gmail.com>
2014-01-27 21:59:57 +0900

    昔書いたコードが気に食わなかった

--- tweet_stack.rb
+++ tweet_stack.rb
@@ -5,30 +5,30 @@
 # http://opensource.org/licenses/mit-license.php
 
 Plugin.create(:tweet_stack) do
-
   stack = Array.new
 
-  def sys_mes msg
-    Plugin.call(:update, nil, [Message.new(:message => msg, :system => true)])
+  def sys_mes(msg)
+    Plugin.call(:update, nil, [Message.new(message: msg, system: true)])
   end
 
-  command(:tweet_stack,
-           name: 'tweet stack',
-           condition: lambda{ |opt| true },
-           visible: true,
-           role: :postbox) do |opt|
+  command(
+    :tweet_stack,
+    name: 'tweet stack',
+    condition: lambda{ |opt| true },
+    visible: true,
+    role: :postbox
+  ) do |opt|
     text = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
     if text.empty? then
       if stack.empty? then
-        sys_mes 'stackが空です'
+        sys_mes('stackが空です')
       else
         Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = stack.pop
       end
     else
-      stack.push text
+      stack.push(text)
       Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text = ''
-      sys_mes "「#{stack}」が積まれています"
+      sys_mes("「#{stack}」が積まれています")
     end
   end
-
 end
